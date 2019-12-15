ccfe100919728ebddd2243d30cdf04a9c30caf8a
mfsuve <suve@itu.edu.tr>
2019-12-15 10:56:30 +0300

    Put in cpp folder and using TabuSearch class

---
+++ C++/cpp/vrp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include "tabusearch.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " filename";
+        return 0;
+    }
+    srand(0);
+    TabuSearch solver(argv[1]);
+    solver.solve(5);
+}
