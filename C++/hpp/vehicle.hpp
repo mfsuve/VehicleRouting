d5cc6ea78faa77534e9d7ec83d87a682d74403da
Suve <Mustafa.Suve@adesso.com.tr>
2019-12-19 19:05:19 +0300

    fixed header file

--- C++/hpp/vehicle.hpp
+++ C++/hpp/vehicle.hpp
@@ -2,23 +2,25 @@
 #define VEHICLE_HPP
 #include <iostream>
 #include <list>
+#include "../hpp/customer.hpp"
 
 using namespace std;
 
 class Vehicle {
 public:
     list<Customer> route;
-    Vehicle();
+    Vehicle(int capacity);
     bool fits(int demand) const;
     bool fits(Customer& c) const;
     bool fits(list<Customer>::iterator i) const;
     list<Customer>::iterator begin();
     list<Customer>::iterator end();
     Customer remove(list<Customer>::iterator i);
-    void add(Customer node, list<Customer>::iterator i);
+    void add(Customer& node, list<Customer>::iterator i);
+    void add(Customer& node);
     bool empty();
-    static int capacity;
 private:
+    int capacity;
     int load;
 };
 #endif
