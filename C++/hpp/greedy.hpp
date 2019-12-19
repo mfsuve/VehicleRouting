cd736dc9e813058b598cb328cfb89bae060bbe18
mfsuve <suve@itu.edu.tr>
2019-12-19 07:57:30 +0300

    Created Greedy class

--- C++/hpp/greedy.hpp
+++ C++/hpp/greedy.hpp
@@ -1,8 +1,18 @@
-// #include <iostream>
+#include <iostream>
+#include <vector>
+#include "../hpp/customer.hpp"
+#include "../hpp/vehicle.hpp"
 
-// using namespace std;
+using namespace std;
 
-// class Greedy {
-// public:
-//     Greedy()
-// }
+class Greedy {
+public:
+    int cost;
+    Greedy(int N, int V, vector<Customer>& customers, Vehicle vehicle[]);
+    Greedy& solve();
+    double bestNeighbor(const Customer** candidate, const Customer& current, const Vehicle& vehicle);
+private:
+    int numUnvisited, N, V;
+    Vehicle* vehicles;
+    vector<Customer>* customers;
+};
