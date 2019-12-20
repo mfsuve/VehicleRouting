f6be8851019f48a76f4384982496ce06a8182974
mfsuve <suve@itu.edu.tr>
2019-12-20 07:44:48 +0300

    Cleaning, fixed changing customers unintentionally

--- C++/cpp/greedy.cpp
+++ C++/cpp/greedy.cpp
@@ -6,13 +6,15 @@
 #include "../hpp/customer.hpp"
 #include "../hpp/vehicle.hpp"
 
+#include <iomanip>
+
 using namespace std;
 
 Greedy::Greedy(int N, int V) : cost(0), N(N), V(V), numUnvisited(N - 1) {}
 
 pair<double, list<Customer>::iterator> Greedy::bestNeighbor(list<Customer>& customers, Customer& current, Vehicle& vehicle) {
     double minCost = numeric_limits<double>::max(), candidateCost;
-    pair<double, list<Customer>::iterator> result(0, customers.end());
+    pair<double, list<Customer>::iterator> result(minCost, customers.end());
     for (auto it = customers.begin(); it != customers.end(); ++it) {
         Customer& customer = *it;
         if (!customer.visited && vehicle.fits(customer.demand)) {
@@ -27,51 +29,57 @@ pair<double, list<Customer>::iterator> Greedy::bestNeighbor(list<Customer>& cust
 }
 
 Greedy& Greedy::solve(list<Customer>& customers, vector<Vehicle>& vehicles) {
-    for (int i = 0; i < V; ++i) {
-        Vehicle& vehicle = vehicles[i];
+    for (Vehicle& vehicle : vehicles)
         if (vehicle.empty())
-            vehicle.add(customers.front(), vehicle.begin());
-    }
-
-    // For debugging
+            vehicle.add(customers.front());
     customers.front().visited = true;
-    cout << "After setting warehouse as visited:" << endl;
-    cout << "customers.front().visited: " << customers.front().visited << endl;
-
-    cout << "After adding the first node to all vehicles:" << endl;
-    for (int i = 0; i < V; ++i) {
-        Vehicle& vehicle = vehicles[i];
-        cout << "Vehicle size: " << vehicle.route.size() << endl;
-    }
-    // Remove after
 
     int vehicleIndex = 0;
-    Customer& current = customers.front();  // currentLocation is warehouse
+    list<Customer>::iterator current = customers.begin();  // currentLocation is warehouse
     while (numUnvisited > 0) {
         Vehicle& vehicle = vehicles[vehicleIndex];
 
-        auto cost_customer = bestNeighbor(customers, current, vehicle);
+        auto cost_customer = bestNeighbor(customers, *current, vehicle);
         double minCost = cost_customer.first;
         auto candidate = cost_customer.second;
 
         if (candidate == customers.end()) {
+            cout << "No customer fits to Vehicle " << vehicleIndex << ", ";
             // No customer fits
             if (vehicleIndex + 1 < V) { // there are still vehicles
-                if (!current.isWarehouse()) {
-                    vehicle.add(customers.front(), vehicle.end());
-                    cost += current.distance(0);
+                if (!current->isWarehouse()) {
+                    vehicle.add(customers.front());
+                    cost += current->distance(0);
+                    cout << "Vehicle " << vehicleIndex << " returns to warehouse and ";
                 }
                 ++vehicleIndex;
+                current = customers.begin();
+                cout << "going to the next vehicle" << endl;
             }
             else {
                 // We DO NOT have any more vehicle to assign. The problem is unsolved under these parameters
-                cout << "The rest customers do not fit in any Vehicle, the problem cannot be resolved under these constrains" << endl;
+                cout << "The rest customers do not fit in any Vehicle, the problem cannot be resolved under these constrains!" << endl;
                 exit(EXIT_FAILURE);
             }
         }
         else {
-            vehicle.add(*candidate, vehicle.end())
+            cout << "Vehicle " << vehicleIndex << " goes to Customer " << candidate->id << " at (" << candidate->x << ", " << candidate->y << ") with cost of " << minCost << endl;
+            vehicle.add(candidate);
+            current = candidate;
+            candidate->visited = true;
+            cost += minCost;
+            --numUnvisited;
+            cout << "       Load of Vehicle " << vehicleIndex << ": " << vehicle.getLoad() << endl;
+            cout << "       " << numUnvisited << " customers remaining..." << endl;
         }
+
+        // cout << "               customers.size(): " << customers.size() << endl;
+        // for (Customer& customer : customers) {
+        //     if (customer.visited)
+        //         cout << "               Customer " << customer.id << " is visited" << endl;
+        //     else
+        //         cout << "               Customer " << customer.id << " is not visited" << endl;
+        // }
 
     }
     cout << "Vehicle " << vehicleIndex << " returns to warehouse with cost " << current->distance(0) << endl;
