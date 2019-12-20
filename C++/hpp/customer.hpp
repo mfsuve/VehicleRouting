eeaf39fa31fdc27b636c49a8cf2f646a009e5971
mfsuve <suve@itu.edu.tr>
2019-12-20 07:47:39 +0300

    New distance, getX and getY functions

--- C++/hpp/customer.hpp
+++ C++/hpp/customer.hpp
@@ -2,6 +2,7 @@
 #define CUSTOMER_HPP
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -9,12 +10,15 @@ class Customer {
 public:
     int id, demand, visited;
     Customer(int id, int demand, double x, double y);
+    double distance(list<Customer>::iterator i) const;
     double distance(const Customer& to) const;
     double distance(int i) const;
     void addNeighbor(Customer& neighbor);
     bool operator==(const Customer& customer);
     bool isWarehouse() const;
-    vector<double> distances;
+    vector<double> distances;   // TODO make this private
+    int getX();
+    int getY();
 private:
     double x, y;
 };
