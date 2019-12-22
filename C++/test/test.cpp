1fcd5c976190662b6da14d88500a5724e538db8c
mfsuve <suve@itu.edu.tr>
2019-12-22 14:21:57 +0300

    Modifiying test.cpp

--- C++/test/test.cpp
+++ C++/test/test.cpp
@@ -4,12 +4,7 @@
 using namespace std;
 
 int main() {
-    list<int> ints;
-    for (int i = 0; i < 5; ++i)
-        ints.push_back(i);
-    
-    list<int>::iterator it = ints.begin();
-
-    list<int>::iterator it2 = it;
-
+    int j = 0;
+    for (int i = 0; i < 200000; i++)
+        j++;
 }
